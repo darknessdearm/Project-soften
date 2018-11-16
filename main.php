2021c77e992b529ef226b25d9f358b9a28f3b933
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-11-16 23:23:43 +0700

    add new

--- main.php
+++ main.php
@@ -4,28 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Welcome</title>
-    <style>
-        @import "main-style.css";
-        div#member{
-            float: right;
-        }
-    </style>
 </head>
 <body>
-    <div class="topnav">
-    <a href="#home">Home</a>
-    <a href="#recipe">Recipe</a>
-    <a class="active" href="#product">Product</a>
-    <a href="#promotion">Promotion</a>
-    <a href="#contact">Contact</a>
-    <a href="#member">Name</a>
+    <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Recipe</a></li>
+        <li><a href="#">Product</a></li>
+        <li><a href="#">Promotion</a></li>
+        <li><a href="#">Contact</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Username<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+            <li><a href="#">Bookmark</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Log Out</a></li>
+            </ul>
+        </li>
+        </ul>
     </div>
+    </nav>
 
-    <div style="padding-left:16px">
-    <h2>Product</h2>
-    <p>Some content..</p>
-    </div>
 
 </body>
 </html>
