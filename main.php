9253ee6be44298a699d665275349dddb9b0a7c8f
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-11-16 20:55:14 +0700

    Initial commit

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <style>
+        @import "main-style.css";
+        div#member{
+            float: right;
+        }
+    </style>
+</head>
+<body>
+    <div class="topnav">
+    <a href="#home">Home</a>
+    <a href="#recipe">Recipe</a>
+    <a class="active" href="#product">Product</a>
+    <a href="#promotion">Promotion</a>
+    <a href="#contact">Contact</a>
+    <a href="#member">Name</a>
+    </div>
+
+    <div style="padding-left:16px">
+    <h2>Product</h2>
+    <p>Some content..</p>
+    </div>
+
+</body>
+</html>
