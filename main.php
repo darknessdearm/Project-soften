d05627f77148628e2ce30952f11d5f62c790c911
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-11-19 23:47:50 +0700

    add filter btn

--- main.php
+++ main.php
@@ -13,6 +13,7 @@
     <title>Welcome</title>
     <style>
         @import "sildeshow.css";
+        @import "filter.css";
         .nav-link{
            color: white; 
         }
@@ -31,7 +32,6 @@
         .container {
             padding: 2px 16px;
         }
-
     </style>
 </head>
 <body>
@@ -75,6 +75,22 @@
             <div class="col-sm-2"></div>
         </div>
         <br>
+        <div class = "row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <div id="myBtnContainer">
+                <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+                <button class="btn" onclick="filterSelection('pasta')"> Pasta</button>
+                <button class="btn" onclick="filterSelection('oil')"> Oliver Oil</button>
+                <button class="btn" onclick="filterSelection('soup')"> Soup</button>
+                <button class="btn" onclick="filterSelection('psauce')"> Pasta Sauce</button>
+                <button class="btn" onclick="filterSelection('ssauce')"> Soy Sauce</button>
+                <button class="btn" onclick="filterSelection('spice')"> Spices</button>
+                </div>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+        <br>
         <div class="row" style = "margin-left: 5%">
         <div class="col" style = "float:right">
             <div class="card" style="width:400px">
@@ -135,5 +151,51 @@
         </div>
         </div>
         </div>
+        <br>
+        <script>
+        filterSelection("all")
+        function filterSelection(c) {
+        var x, i;
+        x = document.getElementsByClassName("filterDiv");
+        if (c == "all") c = "";
+        for (i = 0; i < x.length; i++) {
+            w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        }
+        }
+
+        function w3AddClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+        }
+        }
+
+        function w3RemoveClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);     
+            }
+        }
+        element.className = arr1.join(" ");
+        }
+
+        // Add active class to the current button (highlight it)
+        var btnContainer = document.getElementById("myBtnContainer");
+        var btns = btnContainer.getElementsByClassName("btn");
+        for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+        }
+        </script>
+
 </body>
 </html>
