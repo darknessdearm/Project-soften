b043b22c002d1948abaa511295cddfa7273e64dc
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-11-19 14:26:45 +0700

    ahhhhhhhhh

--- main.php
+++ main.php
@@ -4,38 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <title>Welcome</title>
     <style>
-        
+        .nav-link{
+           color: white; 
+        }
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <ul class="nav navbar-nav">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#recipe">Recipe</a></li>
-        <li><a href="#product">Product</a></li>
-        <li><a href="#promotion">Promotion</a></li>
-        <li><a href="#contact">Contact</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Username <span class="caret"></span></a>
+    <nav class="navbar navbar-expand-sm navbar-dark fixed-top" style = "background-color: #FF0D1D">
+        <ul class="navbar-nav">
+        <li class = "nav-item"><a class = "nav-link" href="#home">Home</a></li>
+        <li class = "nav-item"><a class = "nav-link" href="#recipe">Recipe</a></li>
+        <li class = "nav-item"><a class = "nav-link" href="#product">Product</a></li>
+        <li class = "nav-item"><a class = "nav-link" href="#promotion">Promotion</a></li>
+        <li class = "nav-item"><a class = "nav-link" href="#contact">Contact</a></li>
+        
+        <li class="nav-item dropdown "><a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Username </a>
             <ul class="dropdown-menu">
-            <li><a href="#">Bookmark</a></li>
-            <li><a href="#">My Purchases</a></li>
-            <li><a href="#">Setting</a></li>
-            <li><a href="#">Log Out</a></li>
+            <li><a class="dropdown-item" href="#">Bookmark</a></li>
+            <li><a class="dropdown-item" href="#">My Purchases</a></li>
+            <li><a class="dropdown-item" href="#">Setting</a></li>
+            <li><a class="dropdown-item" href="#">Log Out</a></li>
             </ul>
         </li>
-        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+        <li class = "nav-item"><a href="#" class='fas fa-shopping-cart' style='font-size:24px; color: white; float: right'></a></li>
         </ul>
-    </div>
     </nav>
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+            <br>
+            <h1 style = "text-align: center; color: tomato">Product</h2>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+        <div class="row">
 
+        </div>
+        <div class="row">
+        </div>
 
 </body>
 </html>
