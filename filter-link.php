934b1cc1d7196232589789267963d762ff7fb98d
Phurithat <phurithat.59070501090@mail.kmutt.ac.th>
2018-11-28 14:32:46 +0700

    asfad

--- filter-link.php
+++ filter-link.php
@@ -7,9 +7,17 @@ else
 $sql = "SELECT * FROM product where Type = '$filter'";
 $result = connect_database($sql);
 
+$sql_bookmark = "SELECT * FROM bookmark where UserID = '1'";
+$result_bookmark = connect_database($sql_bookmark);
+while($rs = $result_bookmark->fetch_assoc()){
+    $product[$rs["ProductID"]] = 1;
+    //echo $rs["ProductID"]."= ".$product[$rs["ProductID"]]."<br>";
+}
 $outp = "[";
 while($rs = $result->fetch_assoc()) {
     if ($outp != "[") {$outp .= ",";}
+    
+
     $outp .= '{"ProductID":"'.$rs["ProductID"].'",';
     $outp .= '"ProductName":"'.$rs["ProductName"].'",';
     $outp .= '"Type":"'.$rs["Type"].'",';
@@ -17,7 +25,10 @@ while($rs = $result->fetch_assoc()) {
     $outp .= '"Description":"'.$rs["Description"].'",';
     $outp .= '"Balance":"'.$rs["Balance"].'",';
     $outp .= '"img":"img/Product/'.$rs["img"].'.png",';
-    $outp .= '"Bookmark":"'.$rs["UserID"].'"}';
+    if(isset($product[$rs["ProductID"]]) && $product[$rs["ProductID"]] == 1)
+    $outp .= '"Bookmark":"1"}';
+    else
+    $outp .= '"Bookmark":""}';
 }
 $outp .="]";
 
